9cb767b3c097a80015dec6e665eb4a98214c2c2d
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2018-02-28 15:57:35 +0100

    works

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class DatabaseService {
 
-  constructor(private firestore: AngularFirestore, private db: AngularFireDatabase) { }
+  constructor(public router: Router, private db: AngularFirestore) { }
 
   getUserList(){
-    return this.firestore.collection('/users').valueChanges();
+    return this.db.collection('/users').valueChanges();
   }
 
   addNewUser(userId, name, email){
-    this.db.list('/users').push({userId, name, email});
+    const newUser = {
+      userId: userId,
+      name: name,
+      email: email
+    }
+
+    this.db.collection('users').add(newUser)
+    .then(user => {
+      console.log('Usuario añadido correctamente!')
+      this.router.navigate(['/userlist']);
+    })
+    .catch(err => console.log(err));
   }
 
 }
